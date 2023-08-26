79a8be28ff0a531724653eeccba9764f1260c833
GHMatrix <gsmensah@yahoo.com>
2023-08-26 11:04:50 +0000

    Remove save methods from BaseModel init

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,10 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
+    def all(self):
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        return FileStorage.__objects
         if cls is None:
             return FileStorage.__objects
         else:
@@ -22,7 +24,6 @@ class FileStorage:
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
-
     def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
@@ -31,7 +32,6 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
-
     def reload(self):
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
@@ -41,7 +41,6 @@ class FileStorage:
         from models.city import City
         from models.amenity import Amenity
         from models.review import Review
-
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
@@ -52,6 +51,7 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
+                        self.all()[key] = classes[val['__class__']](**val)
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
