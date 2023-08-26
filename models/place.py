9128ec58076a345bdbc3f64285a9fbc17feef07c
GHMatrix <gsmensah@yahoo.com>
2023-08-26 21:27:15 +0000

    Updated all classes to actually be classes

--- models/place.py
+++ models/place.py
@@ -1,42 +1,10 @@
 #!/usr/bin/python3
-"""Place Module for HBNB project."""
-from sqlalchemy import Column, String, Float, Integer, ForeignKey, Table
-from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
-from models.amenity import Amenity
-from os import getenv
+""" Place Module for HBNB project """
+from models.base_model import BaseModel
 
 
-class Place(BaseModel, Base):
-    """A place to stay."""
-    __tablename__ = 'places'
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        place_amenity = Table('place_amenity', Base.metadata,
-                              Column('place_id', String(60), ForeignKey('places.id'),
-                                     primary_key=True, nullable=False),
-                              Column('amenity_id', String(60), ForeignKey('amenities.id'),
-                                     primary_key=True, nullable=False))
-        amenities = relationship('Amenity', secondary=place_amenity, back_populates='place_amenities')
-
-    else:
-        @property
-        def amenities(self):
-            """Getter method for amenities."""
-            amenity_list = []
-            for amenity_id in self.amenity_ids:
-                key = 'Amenity.' + amenity_id
-                if key in FileStorage.__objects:
-                    amenity_list.append(FileStorage.__objects[key])
-            return amenity_list
-
-        @amenities.setter
-        def amenities(self, obj):
-            """Setter method for amenities."""
-            if isinstance(obj, Amenity):
-                self.amenity_ids.append(obj.id)
-
-
+class Place(BaseModel):
+    """ A place to stay """
     city_id = ""
     user_id = ""
     name = ""
@@ -48,4 +16,3 @@ class Place(BaseModel, Base):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
-
