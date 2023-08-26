43977903574ad16d8ca3d4eafa0bc47a34e91726
GHMatrix <gsmensah@yahoo.com>
2023-08-26 21:28:46 +0000

    Correct style errors

--- models/amenity.py
+++ models/amenity.py
@@ -1,24 +1,7 @@
 #!/usr/bin/python3
-"""Amenity Module for HBNB project."""
-from sqlalchemy import Column, String, Table, ForeignKey
-from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
+""" State Module for HBNB project """
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    """Amenity class."""
-    __tablename__ = 'amenities'  # Table name
-
-    name = Column(String(128), nullable=False)  # Column for amenity name
-
-    # Relationship with Place using a Many-to-Many relationship
-    place_amenities = Table(
-        'place_amenity',
-        Base.metadata,
-        Column('place_id', String(60), ForeignKey('places.id'),
-               primary_key=True, nullable=False),
-        Column('amenity_id', String(60), ForeignKey('amenities.id'),
-               primary_key=True, nullable=False)
-    )
-
-    places = relationship("Place", secondary=place_amenities, back_populates="amenities")
+class Amenity(BaseModel):
+    name = ""
