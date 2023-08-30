75491c1f9a986ad3eddfb9aa4ba83ac78f884497
CARTOON01 <brianngugi224@gmail.com>
2023-08-30 09:42:49 +0300

    indention

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -20,11 +20,11 @@ class DBStorage:
     def __init__(self):
         """Constructor"""
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
-                                      format(getenv('HBNB_MYSQL_USER'),
-                                             getenv('HBNB_MYSQL_PWD'),
-                                             getenv('HBNB_MYSQL_HOST'),
-                                             getenv('HBNB_MYSQL_DB')),
-                                      pool_pre_ping=True)
+                                        format(getenv('HBNB_MYSQL_USER'),
+                                                getenv('HBNB_MYSQL_PWD'),
+                                                getenv('HBNB_MYSQL_HOST'),
+                                                getenv('HBNB_MYSQL_DB')),
+                                        pool_pre_ping=True)
         if getenv('HBNB_ENV') == "test":
             Base.metadata.drop_all(self.__engine)
 
