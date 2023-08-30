fb771c8f073df2eda8106231a277904f12e169fc
CARTOON01 <brianngugi224@gmail.com>
2023-08-30 10:07:18 +0300

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,66 +1,63 @@
 #!/usr/bin/python3
-""" Test delete feature
-"""
-from models.engine.file_storage import FileStorage
-from models.state import State
+"""This module defines a class to manage file storage for hbnb clone"""
+import json
 
-fs = FileStorage()
 
-# All States
-all_states = fs.all(State)
-print("All States: {}".format(len(all_states.keys())))
-for state_key in all_states.keys():
-    print(all_states[state_key])
+class FileStorage:
+    """This class manages storage of hbnb models in JSON format"""
+    __file_path = 'file.json'
+    __objects = {}
 
-<<<<<<< HEAD
-# Create a new State
-new_state = State()
-new_state.name = "California"
-fs.new(new_state)
-fs.save()
-print("New State: {}".format(new_state))
-=======
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls is None:
-            return FileStorage.__objects
-        else:
-            filtered_objects = {}
-            for key, obj in FileStorage.__objects.items():
-                if isinstance(obj, cls):
-                    filtered_objects[key] = obj
-            return filtered_objects
->>>>>>> refs/remotes/origin/master
 
-# All States
-all_states = fs.all(State)
-print("All States: {}".format(len(all_states.keys())))
-for state_key in all_states.keys():
-    print(all_states[state_key])
+        if cls:
+            new_dict = {}
+            new_list = list(str(cls).split('.'))
+            class_name = new_list[-1].replace("'>", '')
+            for key, value in FileStorage.__objects.items():
+                if class_name in key:
+                    new_dict[key] = value
+            return new_dict
+        return FileStorage.__objects
 
-# Create another State
-another_state = State()
-another_state.name = "Nevada"
-fs.new(another_state)
-fs.save()
-print("Another State: {}".format(another_state))
+    def new(self, obj):
+        """Adds new object to storage dictionary"""
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
-# All States
-all_states = fs.all(State)
-print("All States: {}".format(len(all_states.keys())))
-for state_key in all_states.keys():
-    print(all_states[state_key])        
+    def save(self):
+        """Saves storage dictionary to file"""
+        with open(FileStorage.__file_path, 'w') as f:
+            temp = {}
+            temp.update(FileStorage.__objects)
+            for key, val in temp.items():
+                temp[key] = val.to_dict()
+            json.dump(temp, f)
 
-<<<<<<< HEAD
-# Delete the new State
-fs.delete(new_state)
+    def close(self):
+        """Calls reload() method for deserializing the JSON file to objects"""
+        self.reload()
 
-# All States
-all_states = fs.all(State)
-print("All States: {}".format(len(all_states.keys())))
-for state_key in all_states.keys():
-    print(all_states[state_key])
-=======
+    def delete(self, obj=None):
+        """to delete obj from __objects if it’s inside"""
+        if obj:
+            c_name = obj.__class__.__name__
+            c_id = obj.id
+            key = c_name + "." + c_id
+            if key in FileStorage.__objects:
+                del (self.all()[key])
+                self.save()
+
+    def reload(self):
+        """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
@@ -74,12 +71,3 @@ for state_key in all_states.keys():
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-
-    def delete(self, obj=None):
-        """Deletes obj from __objects if it's inside"""
-        if obj is not None:
-            key = obj.__class__.__name__ + '.' + obj.id
-            if key in FileStorage.__objects:
-                del FileStorage.__objects[key]
-                self.save()
->>>>>>> refs/remotes/origin/master
