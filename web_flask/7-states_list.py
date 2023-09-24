496b11e363a17eab3bd60f67a048c3e45486122d
GHMatrix <gsmensah@yahoo.com>
2023-09-24 21:20:41 +0000

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,6 +10,7 @@ from models import *
 
 app = Flask(__name__)
 
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """
@@ -18,12 +19,14 @@ def states_list():
     states = sorted(storage.all(State).values(), key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def close_session(exception):
     """
     Closes the SQLAlchemy session after each request.
     """
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
