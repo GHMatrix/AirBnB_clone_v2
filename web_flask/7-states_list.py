ea64625a46dd788291f05a117c7d0b20633213b1
GHMatrix <gsmensah@yahoo.com>
2023-09-24 21:17:13 +0000

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,6 +6,7 @@ Starts a Flask web application that displays a list of states.
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models import *
 
 app = Flask(__name__)
 
